0688939bbae0777a8d13735e6e74fe03d36c63e7
spicydog <spicydog@gmail.com>
2015-08-24 22:07:57 +0700

    Release version 1

--- LongdoMapView.h
+++ LongdoMapView.h
@@ -9,31 +9,17 @@
 #import <UIKit/UIKit.h>
 #import "LongdoMapViewDelegate.h"
 
-
-// Layer classes
-@class LongdoMapView;
-@class Layer;
-@class LayerCollection;
-
-// Overlay Marker Classes
-@class Marker;
-@class Popup;
-
-// Search Class
-@class Search;
-
-// Geometric Classes
-@class Polyline;
-
-// Layer Class
-@class Layer;
+// Start Lat/Lon/Zoom
+#define DEFAULT_LAT 13.722439
+#define DEFAULT_LON 100.529451
+#define DEFAULT_ZOOM 14
+#define DEFAULT_MIN_ZOOM 4
+#define DEFAULT_MAX_ZOOM 18
 
 // STATUS CODE
 #define STATUS_OK 200
 #define STATUS_INCORRECT_API_KEY 451
 #define STATUS_UNABLE_TO_CHECK_API_KEY 452
-
-
 
 // Base layer names
 #define LAYER_NAME_BASE @"BASE"
@@ -72,102 +58,128 @@
 #define PROJECTION_EPSG4326 @"EPSG:3857"
 #define PROJECTION_EPSG3857 @"EPSG:4326"
 
+// Longdo Map SDK config
+// Please do not change if you do not know what you are doing.
+#define TILE_CACHE_POOL_SIZE 4
+#define TILE_DOWNLOAD_POOL_SIZE 4
+#define TAG_POOL_SIZE 2
+#define OUT_SCREEN_TILE_MARGIN 0
+#define OUT_SCREEN_TILE_REMOVE_MARGIN 0
+#define ZOOM_PERIOD 15
+#define MOVE_PERIOD 15
+#define TAG_EXTRA_SIZE 0
+#define ONLY_DRAW_OVERLAY_LAYER_ON_BASE_IS_READY YES
+#define FADE_IN_BASE_LAYER YES
 
-@interface LongdoMapView : UIView <LongdoMapViewDelegate> {
-    
+// Layer classes
+@class LongdoMapView;
+@class Layer;
+@class MapLayers;
+
+// Overlay Marker Classes
+@class Marker;
+@class Popup;
+
+// Search Class
+@class Search;
+
+// Geometric Classes
+@class Polyline;
+
+// Layer Class
+@class Layer;
+
+@class Tile;
+
+
+// Struct
+//typedef struct {
+//  double latitude;
+//  double longitude;
+//} MapLocation;
+
+
+@interface LongdoMapView : UIView {
 }
 
+@property(retain) id<LongdoMapViewDelegate> longdoMapViewDelegate;
 
-@property (retain) id <LongdoMapViewDelegate> delegate;
-
-// public
-
+// Initialization
+- (void)initMap; // without key, you need your own map server
 - (void)initMapWithKey:(NSString *)accessKey;
+- (void)closeMap;
 
-- (void)setAccessKey:(NSString *)accessKey;
+// Map control
+- (void)refresh;
 
+// Map parameters
+- (void)setMaxZoomLevel:(short)maxZoom;
+- (void)setMinZoomLevel:(short)minZoom;
 - (void)setMapBackgroundColor:(UIColor *)backgroundColor;
 
+- (void)enablePan:(bool)isEnable;
+- (void)enablePinch:(bool)isEnable;
 - (void)enableTilt:(bool)isEnable;
 - (void)enableRotate:(bool)isEnable;
 - (void)enableCenterZooming:(bool)isEnable;
 - (void)enableCrossMark:(bool)isEnable;
 
+// Location
+- (double)latitude;
+- (double)longitude;
+- (CLLocationCoordinate2D)coordinate;
+- (CLLocationCoordinate2D)location;
+- (void)setLocation:(CLLocationCoordinate2D)coordinate; // Without animation
+- (void)setLocation:(CLLocationCoordinate2D)coordinate
+      withAnimation:(bool)isAnimate;
 
-- (void)animateScaleBarAlphaTo:(float)alpha withAnimationDuration:(float)seconds;
+// Zoom
+- (int)zoom;
+- (void)zoomIn;
+- (void)zoomOut;
+- (void)setZoom:(int)targetZoom; // Without animation
+- (void)setZoom:(int)targetZoom withAnimation:(bool)isAnimate;
+
+// Bound
+- (CGRect)getCoordinateBoundaryForZoom:(int)zoom
+                              atCenter:(CLLocationCoordinate2D)coordinate;
+- (void)setMapBoundaryFor:(CGRect)boundary;
+- (void)setBoundBetween:(CLLocationCoordinate2D)coordinate1
+                    and:(CLLocationCoordinate2D)coordinate2
+          withExtraZoom:(int)extraZoom; // deprecate
+- (void)setBoundBetween:(CLLocationCoordinate2D)coordinate1
+                    and:(CLLocationCoordinate2D)coordinate2
+             withCenter:(CLLocationCoordinate2D)center
+           andExtraZoom:(int)extraZoom; // deprecate
+
+// Marker
+- (NSArray *)markers;
+- (void)addMarker:(Marker *)marker;
+- (void)removeMarker:(Marker *)marker;
+- (void)removeMarkerWithTag:(NSString *)tag;
+- (void)removeAllMarkers;
+
+// Geometric Object
+- (void)removeGeometricObjectWithTag:(NSString *)tag;
+- (void)removeAllGeometricObjects;
+
+// Longdo Tag
+- (void)showLongdoTags:(NSArray *)longdoTags;
+- (void)hideLongdoTags;
+
+// Scale bar
+- (void)animateScaleBarAlphaTo:(float)alpha
+         withAnimationDuration:(float)seconds;
 - (void)enableScaleBar:(bool)isEnable;
 - (void)setScaleBarMarginX:(int)x andMarginY:(int)y;
 - (void)setScaleBarUpSideDown:(bool)isInvert;
 - (void)setScaleBarRightToLeft:(bool)isInvert;
 - (void)setScaleBarAlpha:(float)alpha;
-- (void)setScaleBarColorRed:(float)red Green:(float)green Blue:(float)blue;
-
-
-- (void)refresh;
-
-- (double)cX;
-- (double)cY;
-- (CGPoint)cXY;
-- (void)gotoXY:(CGPoint)newXY;
-- (void)setXY:(CGPoint)newXY;
-
-
-- (double)latitude;
-- (double)longitude;
-- (CLLocationCoordinate2D)coordinate;
-- (CLLocationCoordinate2D)location;
-- (void)setLocation:(CLLocationCoordinate2D)coordinate withAnimation:(bool)isAnimate;
-- (void)setLocation:(CLLocationCoordinate2D)coordinate;  // Without animation
-- (void)gotoLocation:(CLLocationCoordinate2D)coordinate; // With animation
-
-- (void)trackUserLocation;
-- (void)stopTrackUserLocation;
-
-
-- (int)zoom;
-- (void)zoomIn;
-- (void)zoomOut;
-- (void)gotoZoom:(int)targetZoom; // Without animation
-- (void)setZoom:(int)targetZoom;  // Without animation
-- (void)setZoom:(int)targetZoom withAnimation:(bool)isAnimate;
-- (void)setMaxZoomLevel:(short)maxZoom;
-- (void)setMinZoomLevel:(short)minZoom;
-- (void)setBoundBetween:(CLLocationCoordinate2D)coordinate1 and:(CLLocationCoordinate2D)coordinate2;
-- (void)setBoundBetween:(CLLocationCoordinate2D)coordinate1 and:(CLLocationCoordinate2D)coordinate2 withCenter:(CLLocationCoordinate2D)center;
-
-- (NSArray *)getAllOverlayObjects;
-- (void)removeOverlayObjectWithTag:(NSString*)tag;
-- (void)removeAllOverlayObjects;
-
-- (void)removeGeometricObjectWithTag:(NSString *)tag;
-- (void)removeAllGeometricObjects;
-
-- (void)disableGestureZoomIn;
-- (void)disableGestureZoomOut;
-
-- (void)showPOIs:(NSArray *)poiTags;
-
-- (NSArray *)search:(Search *)query;
-- (NSArray *)suggest:(NSString *)keyword;
-
-- (void)addOverlayObject:(id)object;
-- (void)addGeometicObject:(id)object;
-
-
-// Traffic Features
-- (void)showTrafficCamera;
-- (void)showTrafficCameraBetweenMinZoom:(int)minZoom andMaxZoom:(int)maxZoom;
-- (void)hideTrafficCamera;
-
-- (void)showTrafficEvents;
-- (void)showTrafficEventsBetweenMinZoom:(int)minZoom andMaxZoom:(int)maxZoom;
-- (void)hideTrafficEvents;
-
+- (void)setScaleBarColorRed:(float)red green:(float)green blue:(float)blue;
 
 // Map Layer
 - (Layer *)layerName:(NSString *)name;
 - (void)addLayer:(Layer *)layer name:(NSString *)name;
-
 
 // Layer Collection
 - (Layer *)baseLayer;
@@ -177,45 +189,58 @@
 - (void)removeOverlayLayer:(Layer *)layer;
 - (void)removeAllOverlayLayers;
 
+// Gesture control
+- (void)disableGestureZoomIn;
+- (void)disableGestureZoomOut;
 
-// For engine
-- (void)playAnimation;
-- (void)onCameraUpdate:(NSDictionary*)camera;
-- (void)onEventUpdate:(NSDictionary*)events;
+// Overlay object
+- (void)addGeometicObject:(id)object;
+
+// Debuger
+- (void)setDebugLabel:(UILabel *)label;
 
 @end
-
-
-
-
 
 // Class Marker
 @interface Marker : NSObject
 
-@property CLLocationCoordinate2D coordinate;
-@property (retain) NSString *tag;
-@property (retain) UIImage *icon;
-@property CGPoint offset;
-@property int weight;
-@property float opacity;
-@property bool tapable;
-@property bool persistent;
-@property int minZoom;
-@property int maxZoom;
+- (id)initWithImage:(UIImage *)image andCoordinate:(CLLocationCoordinate2D)coordinate;
+- (id)initWithImageUrl:(NSString *)url andCoordinate:(CLLocationCoordinate2D)coordinate;
 
-- (id)initWithUrlMarker:(NSString *)url atCoordinate:(CLLocationCoordinate2D)coordinate;
-- (id)initWithIconMarker:(UIImage *)icon atCoordinate:(CLLocationCoordinate2D)coordinate;
+
+- (NSString *)tag;
+- (UIImage *)image;
+- (CLLocationCoordinate2D)coordinate;
+- (CGPoint)offset;
+- (int)weight;
+- (int)minZoom;
+- (int)maxZoom;
+- (bool)isClickable;
+- (bool)isPersistent;
+
+
+
+- (Marker *)setTag:(NSString *)tag;
+- (Marker *)setImage:(UIImage *)image;
+- (Marker *)setCoordinate:(CLLocationCoordinate2D)coordinate;
+- (Marker *)setOffset:(CGPoint)offset;
+- (Marker *)setWeight:(int)weight;
+- (Marker *)setMaxZoom:(int)maxZoom;
+- (Marker *)setMinZoom:(int)minZoom;
+- (Marker *)setIsClickable:(bool)isClickable;
+- (Marker *)setIsPersistent:(bool)isPersistent;
+
+
+
 @end
-
-
 
 // Class Popup
 @interface Popup : NSObject
 
 @property CLLocationCoordinate2D coordinate;
-@property (retain) NSString *title;
-@property (retain) NSString *detail;
-@property (retain) NSString *tag;
+@property(retain) NSString *title;
+@property(retain) NSString *detail;
+@property(retain) NSString *tag;
 @property CGPoint offset;
 @property int weight;
 @property float opacity;
@@ -224,79 +249,83 @@
 
 @end
 
-
-
 // Class Polyline
 @interface Polyline : NSObject
 
-
-@property (retain) NSMutableArray *coordinates;
-@property (retain) NSString *tag;
+@property(retain) NSMutableArray *coordinates;
+@property(retain) NSString *tag;
 @property int weight;
 @property float opacity;
-@property (retain) UIColor *lineColor;
+@property(retain) UIColor *lineColor;
 @property int lineWidth;
 
-
-- (id)initWithCoordinates:(NSArray*)coordinates;
+- (id)initWithCoordinates:(NSArray *)coordinates;
 - (void)addCoordinate:(CLLocationCoordinate2D)coordinate;
 
 @end
 
-
 // Class Search
 @interface Search : NSObject
 
-@property (retain) NSString *keyword;
+@property(retain) NSString *keyword;
 @property CLLocationCoordinate2D coordinate;
-@property (retain) NSString *area;
-@property (retain) NSString *tag;
-@property (retain) NSString *language;
+@property(retain) NSString *area;
+@property(retain) NSString *tag;
+@property(retain) NSString *language;
 @property int limit;
 @property int offset;
 @property int span;
 
-- (id)initWithKeyword:(NSString*)keyword;
+- (id)initWithKeyword:(NSString *)keyword;
 
 @end
-
-
 
 // Class Layer
 @interface Layer : NSObject
 
-@property (retain) NSString *name;
-@property (retain) NSString *url;
-@property (retain) NSString *key;
-@property (retain) NSString *format;
-@property (retain) NSString *srs;
-@property (retain) NSString *transparent;
-@property (retain) NSString *styles;
-@property (retain) NSString *extraQuery;
+@property(retain) NSString *name;
+@property(retain) NSString *url;
+@property(retain) NSString *key;
+@property(retain) NSString *format;
+@property(retain) NSString *srs;
+@property(retain) NSString *transparent;
+@property(retain) NSString *styles;
+@property(retain) NSString *extraQuery;
 @property int type;
 @property int refresh;
 @property float opacity;
 @property int weight;
-@property int minZoomRange;
-@property int maxZoomRange;
+@property int minZoom;
+@property int maxZoom;
+@property int maxAge;
 
 - (id)initWithUrl:(NSString *)url andType:(int)type;
-- (NSString *)tileUrlForZoom:(int)zoom atColumn:(int)columnNumber andRow:(int)rowNumber;
-
+- (NSString *)tileUrlForZoom:(int)zoom
+                    atColumn:(int)columnNumber
+                      andRow:(int)rowNumber;
 
 @end
-
-
-
 
 // Class Longdo Tag
 @interface LongdoTag : NSObject
 
-@property (retain) NSString *name;
+@property(retain) NSString *name;
 @property int minZoomRange;
 @property int maxZoomRange;
 
 - (id)initWithName:(NSString *)name;
-- (id)initWithName:(NSString *)name betweenMinZoom:(int)minZoom andMaxZoom:(int)maxZoom;
+- (id)initWithName:(NSString *)name
+    betweenMinZoom:(int)minZoom
+        andMaxZoom:(int)maxZoom;
+
+@end
+
+// Class Tag data
+@interface TagData : NSObject
+
+@property(retain) NSString *ID;
+@property(retain) NSString *name;
+@property(retain) NSString *icon;
+@property CLLocationCoordinate2D coordinate;
 
 @end
--- LongdoMapViewDelegate.h
+++ LongdoMapViewDelegate.h
@@ -10,9 +10,10 @@
 #import "LongdoMapView.h"
 #import <CoreLocation/CoreLocation.h>
 
-
 @class LongdoMapView;
 @class LongdoMapInfo;
+@class TagData;
+@class Marker;
 
 @protocol LongdoMapViewDelegate
 
@@ -20,47 +21,39 @@
 
 @required
 
+
+// Map Status
 - (void)longdoMapView:(LongdoMapView *)mapView SDKStatus:(int)status;
 - (void)longdoMapView:(LongdoMapView *)mapView mapIsReady:(bool)isMapReady;
-- (void)longdoMapView:(LongdoMapView *)mapView statusChangedTo:(LongdoMapInfo *)mapStatus;
-- (void)longdoMapView:(LongdoMapView *)mapView tappedAtIconTag:(NSString *)tag;
-- (void)longdoMapView:(LongdoMapView *)mapView tappedAtCoordinate:(CLLocationCoordinate2D)coordinate;
-- (void)longdoMapView:(LongdoMapView *)mapView longPressedStartAtCoordinate:(CLLocationCoordinate2D)coordinate;
-- (void)longdoMapView:(LongdoMapView *)mapView longPressedFinishAtCoordinate:(CLLocationCoordinate2D)coordinate;
-- (void)longdoMapView:(LongdoMapView *)mapView currentUserCoordinate:(CLLocationCoordinate2D)userCoordinate;
+- (void)longdoMapView:(LongdoMapView *)mapView
+      statusChangedTo:(LongdoMapInfo *)mapStatus;
 
+// Click
+- (void)longdoMapView:(LongdoMapView *)mapView
+      clickedOnMarker:(Marker *)marker;
+- (void)longdoMapView:(LongdoMapView *)mapView
+         clickedOnTag:(TagData *)tagData;
+- (void)longdoMapView:(LongdoMapView *)mapView
+  clickedOnCoordinate:(CLLocationCoordinate2D)coordinate;
+
+// Long Click
+- (void)longdoMapView:(LongdoMapView *)mapView
+    longPressedStartAtCoordinate:(CLLocationCoordinate2D)coordinate;
+- (void)longdoMapView:(LongdoMapView *)mapView
+    longPressedFinishAtCoordinate:(CLLocationCoordinate2D)coordinate;
+
+// Zooming
 - (void)longdoMapView:(LongdoMapView *)mapView startZoomFrom:(int)zoom;
 - (void)longdoMapView:(LongdoMapView *)mapView finishZoomTo:(int)zoom;
 
-- (void)longdoMapView:(LongdoMapView *)mapView startMovingFrom:(CLLocationCoordinate2D)coordinate;
-- (void)longdoMapView:(LongdoMapView *)mapView finishMovingTo:(CLLocationCoordinate2D)coordinate;
+// Moving
+- (void)longdoMapView:(LongdoMapView *)mapView
+      startMovingFrom:(CLLocationCoordinate2D)coordinate;
+- (void)longdoMapView:(LongdoMapView *)mapView
+       finishMovingTo:(CLLocationCoordinate2D)coordinate;
 
-
+// User Action
 - (void)onUserPanning:(LongdoMapView *)mapView;
 - (void)onUserZooming:(LongdoMapView *)mapView;
-- (void)onUserClickPOI:(LongdoMapView *)mapView ID:(NSString*)ID name:(NSString*)name icon:(NSString*)icon atCoordinate:(CLLocationCoordinate2D)coordinate;
-- (void)onUserClickCamera:(LongdoMapView *)mapView
-                 cameraID:(NSString*)camid
-             atCoordinate:(CLLocationCoordinate2D)coordinate
-                    title:(NSString*)title
-                     link:(NSString*)link
-               lastUpdate:(NSString*)lastupdate
-                   incity:(NSString*)incity
-             organization:(NSString*)organization
-              sponsertext:(NSString*)sponsertext
-                   motion:(bool)isMotion
-                   vdoURL:(NSString*)vdourl
-                   imgURL:(NSString*)imgurl
-           imgurlSpecific:(NSString*)imgurlSpecific;
-
-- (void)onUserClickEvent:(LongdoMapView *)mapView
-                 eventID:(NSString*)eventID
-            atCoordinate:(CLLocationCoordinate2D)coordinate
-                   title:(NSString*)title
-            description:(NSString*)description
-                    icon:(NSString*)icon
-                   start:(NSString*)start
-                    stop:(NSString*)stop
-             contributor:(NSString*)contributor;
 
 @end
